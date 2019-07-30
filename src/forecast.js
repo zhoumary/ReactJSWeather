83accfb4b4ce1c3a1770c86842569e2e705d0584
1070604284@qq.com <32705597+zhoumary@users.noreply.github.com>
2019-07-30 18:03:25 +0800

    Add files via upload

---
+++ src/forecast.js
@@ -0,0 +1,88 @@
+import './weather.css';
+import Raining from './icons8-rain-cloud-50.png';
+
+import React, { Component } from 'react'
+
+class Forecast extends Component {
+    getNextWeekDay(i) {
+        const day = new Date();
+        const weeklyNum = day.getDay();
+        var weekday = new Array(7);
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+        weekday[0] = "Sunday";
+        let index = weeklyNum + i + 1;
+        if (index > 6) {
+            index = index % 7;
+        }
+        return weekday[index];
+    }
+
+    renderForeWeather(j, i) {
+        if (j === 0) {
+            return (
+                <td class="forecast">
+                    {this.getNextWeekDay(i)}
+                </td>
+            );
+        } else if (j === 1) {
+            return (
+                <td class="forecast">
+                    <img class="weatherIcon" src={Raining} alt="raining" />
+                </td>
+            );
+        } else if (j === 2) {
+            return (
+                <td class="forecast">
+                    25
+                    <span class="tempUnit">&#8451;</span>
+                </td>
+            );
+        } else if (j === 3) {
+            return (
+                <td class="forecast">
+                    23
+                    <span class="tempUnit">&#8451;</span>
+                </td>
+            );
+        }
+
+    }
+
+    createForecastList(row, column) {
+        let div = [];
+        if (row) {
+            let divCount = 0;
+            for (let i = 0; i < row; i++) {
+                let children = [];
+                //Inner loop to create children
+                for (let j = 0; j < column; j++) {
+                    children.push(this.renderForeWeather(j, i));
+                }
+
+                //Create the parent and add the children
+                div.push(<tr className="forecastRow">{children}</tr>);
+                divCount = divCount + 1;
+            }
+        }
+        return div;
+    }
+
+    render() {
+        return (
+            <div id="forecastTable">
+                <table id="forecastTableBody">
+                    <tbody>
+                        {this.createForecastList(7, 4)}
+                    </tbody>                    
+                </table>
+            </div>
+        )
+    }
+}
+
+export default Forecast
